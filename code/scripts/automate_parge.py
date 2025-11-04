7eddfcac28a88873f5f38cf4268baf619a5ca78d
Ackermann Pascal <F80877978@agsad.admin.ch>
2025-11-04 15:38:16 +0100

    Separated Copy Folder from Parge Automation

--- code/scripts/automate_parge.py
+++ code/scripts/automate_parge.py
@@ -1,47 +1,9 @@
-from pathlib import Path
-import shutil
+
 from pywinauto import Desktop, Application
 from pywinauto.timings import Timings
 import time
 
 Timings.Defaults()
-
-
-# Exclude Steps 05 and 06 because those steps will be done by this script
-excluded_steps = ["05_output_parge",
-                  "06_output_droacor", "06_output_droacor_final"]
-
-# Set your Job
-sel_folders = ["re112o_250903"]
-raw_folder = Path("D:/data/mjolnir")
-process_folder = Path("E:/mjolnir_processing")
-
-
-
-
-def list_folders(path: Path) -> list:
-    folders = [f.name for f in path.iterdir() if f.is_dir()]
-    return folders
-
-
-def copy_folder(origin: Path, dest: Path, folders: list, excluded_subfolder: list):
-    print("Copying Started")
-    for folder in folders:
-        origin_folder = origin / folder
-        dest_folder = dest / folder
-        if not folder_exists(path=dest, folder=folder):
-            print(f"Copying {folder} ...")
-            shutil.copytree(str(origin_folder), str(dest_folder),
-                            ignore=lambda src, names: [name for name in names if name in excluded_subfolder])
-    print("Copying Finished")
-
-
-def folder_exists(path: Path, folder: str) -> bool:
-    existing_folders = list_folders(path)
-    if folder in existing_folders:
-        print(f"'{folder}' already exists!")
-        return True
-    return False
 
 
 '''How to use Pywinauto
@@ -61,9 +23,6 @@ idlvm_window.wait('exists', timeout=1)
 def main():
 
     wait_time = 10
-    copy_folder(origin=raw_folder, dest=process_folder,
-                folders=sel_folders, excluded_subfolder=excluded_steps)
-
 
     # Launch Target Executable
     app = Application(backend="uia").start("C:/ReSe_Software_Win/parge/PARGE.exe",
