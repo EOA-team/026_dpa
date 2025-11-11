f8ba83d2d59617253486aaab17944755563a0536
Ackermann Pascal <F80877978@agsad.admin.ch>
2025-11-11 11:15:55 +0100

    Loading Config Works

--- code/scripts/automate_m4mproc.py
+++ code/scripts/automate_m4mproc.py
@@ -2,6 +2,18 @@ import time
 from pywinauto import Desktop, Application
 from pywinauto.controls.uiawrapper import UIAWrapper
 from pywinauto.timings import Timings
+from pathlib import Path
+import shutil
+from pyprojroot import here
+
+Timings.Fast()
+wait_time = 3
+
+
+
+raw_folder = Path("D:/data/mjolnir")
+process_folder = Path("E:/mjolnir_processing")
+
 
 
 class CheckboxesControl:
@@ -78,10 +90,6 @@ class CheckboxesControl:
         self.state = self.get_active_states()
 
 
-    
-
-Timings.Defaults()
-wait_time = 20
 
 
 def open_idlvm(desktop: Desktop):
@@ -89,8 +97,26 @@ def open_idlvm(desktop: Desktop):
     idlvm_window.wait('exists', timeout=wait_time)
     return idlvm_window
 
+def get_m4mProc(desktop: Desktop):
+    tmp_window = Desktop(backend="uia").window(title='ReSe Hyspex Processor 2025')
+    if tmp_window.exists(timeout=wait_time):
+        print("M4M Window already exists!")
+        window = tmp_window
+    else:
+        print("Window does not yet exist, will be created...")
+        window= open_M4MProc(desktop)
+    
+    return window
+
+
+
 
 def open_M4MProc(desktop: Desktop):
+    # Launch Target Executable
+    app = Application(backend="uia").start("C:/ReSe_Software_Win/m4mproc/M4Mproc.exe",
+                                           work_dir="C:/ReSe_Software_Win/m4mproc/"  # Makes sure config.ini is loaded
+                                           )
+    
     idlvm_window = open_idlvm(desktop)
     pane = idlvm_window.child_window(control_type="Pane")
     pane.wait('exists', timeout=wait_time)
@@ -100,7 +126,69 @@ def open_M4MProc(desktop: Desktop):
 
     m4m_window = desktop.window(title='ReSe Hyspex Processor 2025')
     m4m_window.wait('exists', timeout=wait_time)
+
+    # Bring window to foreground
+    m4m_window.set_focus()
+    m4m_window.click_input()
     return m4m_window
+
+def file_exists(base_path: Path, filename: str) -> bool:
+    return (base_path / filename).is_file()
+
+def load_config_file(filepath: Path, window: UIAWrapper):
+    if not filepath.is_file():
+        print(f"Config File {filepath} does not exist...")
+        src_file = here() / "code" /"configs" / filepath.name
+        dst_file = filepath
+        print(f"Copy from {src_file}")
+        shutil.copy(src_file, dst_file)
+    
+
+        
+    #Expand  Edit Tab 
+    menu_bar = window.child_window(title="Application", control_type="MenuBar")
+    menu_bar.wait('exists', timeout=wait_time)
+    edit_tab = menu_bar.child_window(title="Edit", control_type="MenuItem")
+    edit_tab.wait('exists', timeout=wait_time)
+    edit_tab.expand()
+
+    # Open File Selector
+    edit_menu = window.child_window(title = "Edit", control_type = "Menu")
+    load_config = edit_menu.child_window(title = "Load Configuration", control_type= "MenuItem")
+    load_config.wait('exists', timeout=wait_time)
+    load_config.click_input()
+    
+    #File Selector
+    filesel_dialog = window.child_window(title='Please Select a File', control_type= "Window")
+
+    filesel_dialog.wait('exists', timeout=wait_time) 
+    filesel_dialog.set_focus()
+    filesel_dialog.click_input()
+
+
+     #Access the ComboBox
+    file_combo = filesel_dialog.child_window(title="File name:", control_type="ComboBox")
+    file_combo.wait('exists', timeout=wait_time) 
+
+    #Write Path 
+    file_edit = file_combo.child_window(control_type="Edit")
+    file_edit.wait('exists', timeout=wait_time) 
+    # make sure focus is on the edit
+    file_edit.set_focus()
+    file_edit.click_input()  
+
+    file_edit.type_keys(filepath)
+    file_edit.type_keys("{ENTER}")
+
+
+
+    
+
+    
+
+
+
+
 
 
 
@@ -109,21 +197,19 @@ def open_M4MProc(desktop: Desktop):
 
 
 def main():
-
-    # Launch Target Executable
-    app = Application(backend="uia").start("C:/ReSe_Software_Win/m4mproc/M4Mproc.exe",
-                                           work_dir="C:/ReSe_Software_Win/m4mproc/"  # Makes sure config.ini is loaded
-                                           )
     # Create a Desktop Object to see all windows present on Desktop
     desktop = Desktop(backend="uia")
 
-    m4m_window = open_M4MProc(desktop)
+    m4m_window = get_m4mProc(desktop)
     checkboxes = CheckboxesControl(
         names= ["Raw Data Import", "Geocoding", "Reflectance Retrieval",
                 "Topograhic and Radiometric Correction", "Product Generation", "Rectification", "Mosaic", "Point Cloud"],
         window=m4m_window,
         active=[ "Geocoding","Reflectance Retrieval","Rectification", "Mosaic" ]
     )
+    load_config_file(filepath=process_folder / "conf_rese_prcsr.json", window=m4m_window)
+    m4m_window.set_focus()
+    m4m_window.click_input()
     checkboxes.set_checkboxes()
     
 
