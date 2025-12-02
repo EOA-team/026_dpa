9eff768f151e08a644d456b30732cfa7ff5becd2
Ackermann Pascal <F80877978@agsad.admin.ch>
2025-12-02 15:55:10 +0100

    enable disable radiobuttons and checkboxes

--- code/scripts/automate_hyspexrad.py
+++ code/scripts/automate_hyspexrad.py
@@ -44,11 +44,74 @@ def open_hyspexrad(desktop: Desktop):
     return hyspexrad_window
 
 
+def find_element_by_title(window: UIAWrapper, element_type: str, title: str, partial: bool = True) -> UIAWrapper  :
+    """
+    Returns the first control within the given window
+    that matches the specified title.
+
+    Parameters:
+    - window: UIAWrapper of the parent window.
+    - element_type : "Button, RadioButton, ..."
+    - title: Title of the element to search for.
+    - partial: If True, allows partial (substring) matching.
+
+    Returns:
+    - UIAWrapper object representing the matching Button control,
+      or None if no matching button is found.
+    """
+    normalized_title = title.strip().lower()
+    found_elements = []
+
+    for ctrl in window.descendants():
+        try:
+            if ctrl.element_info.control_type != element_type:
+                continue
+
+            element_text = ctrl.window_text().strip()
+            found_elements.append(element_text)
+
+            # check match
+            element_text_norm = element_text.lower()
+            if partial and normalized_title in element_text_norm:
+                return ctrl
+            elif not partial and normalized_title == element_text_norm:
+                return ctrl
+
+        except Exception:
+            continue
+
+
+
 
 def main():
     # Create a Desktop Object to see all windows present on Desktop
     desktop = Desktop(backend="uia")
     m4m_window = get_hyspexrad(desktop)
+    
+    m4m_window.print_control_identifiers()
+    
+    rb1 = find_element_by_title(window=m4m_window, element_type="RadioButton", partial=True, title="BSQ")
+    rb1.select()
+
+
+    rb2 = find_element_by_title(window=m4m_window, element_type="RadioButton", partial=True, title="32 bit float")
+    rb2.select()
+
+
+    rb3 = find_element_by_title(window=m4m_window, element_type="CheckBox", partial=True, title="radiance ")
+    rb3.invoke()
+
+
+    rb4 = find_element_by_title(window=m4m_window, element_type="CheckBox", partial=True, title="rgb")
+    rb4.invoke()
+
+    rb5 = find_element_by_title(window=m4m_window, element_type="ListItem", partial=True, title="JPG")
+    rb5.select()
+
+    rb5 = find_element_by_title(window=m4m_window, element_type="ListItem", partial=True, title="Envi Mask ")
+    rb5.select()
+
+
     time.sleep(5)
     m4m_window.close()
 
