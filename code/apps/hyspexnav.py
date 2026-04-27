ba9b6309ea2163dbf44e2975456784fb19e26ebd
Ackermann Pascal <F80877978@agsad.admin.ch>
2026-04-27 09:56:36 +0200

    Fixing pylint warnings for runner.py and hyspexnav.py

--- code/apps/hyspexnav.py
+++ code/apps/hyspexnav.py
@@ -7,8 +7,6 @@ from code.pywinauto_helpers import (
 from code.file_utils import get_base_path, wait_for_file_count
 
 
-
-
 class HyspexNavApplication:
     """Automates the HySpex NAV desktop application via pywinauto.
 
@@ -30,7 +28,6 @@ class HyspexNavApplication:
         self.window_auto_id: str = "HySpexNav"
         self.is_idl_application: bool = False
 
-
     def _open_setup_frame(self, controlfinder: ControlFinder) -> None:
         """Open the Setup frame to configure input file patterns.
         The Setup frame defines which file patterns HySpex NAV uses to
@@ -39,18 +36,17 @@ class HyspexNavApplication:
             The Setup control is a CheckBox in UIA but behaves as a toggle button.
         """
         checkbox = controlfinder.find_by_auto_id(
-            control_type="CheckBox", # detected in UIA as checkbox, but it is a button!
+            control_type="CheckBox",  # detected in UIA as checkbox, but it is a button!
             auto_id="HySpexNav.btnSetup")
-        
+
         checkbox_simulator = ControlSimulator(checkbox)
         checkbox_simulator.enable_checkbox()
-
 
     def _click_load_settings_button(self, controlfinder: ControlFinder) -> None:
         """Click the 'Load Settings' button in the Setup frame."""
         btn = controlfinder.find_by_auto_id(
             control_type="Button",
-                auto_id="HySpexNav.frameSetup.btnLoadSettings")
+            auto_id="HySpexNav.frameSetup.btnLoadSettings")
         btn.invoke()
 
     def _open_loadsettings_window(self, controlfinder: ControlFinder) -> None:
@@ -76,7 +72,7 @@ class HyspexNavApplication:
             control_type="Edit", control_name="file name:", exact=True)
         filename_editbox.set_edit_text(str(input_file))
         filename_editbox.type_keys("{ENTER}")
-    
+
         # Wait for window to close
         settings_cf.window.wait_not('exists', timeout=DEFAULT_WAIT_TIME)
 
@@ -84,7 +80,7 @@ class HyspexNavApplication:
         """Open the 'Add Files' window to set the input files"""
         btn = controlfinder.find_by_auto_id(
             control_type="Button",
-                auto_id="HySpexNav.splitter.layoutWidgetMain.btnInputFileAdd")
+            auto_id="HySpexNav.splitter.layoutWidgetMain.btnInputFileAdd")
         btn.invoke()
 
     def _get_addfiles_window(self, controlfinder: ControlFinder) -> UIAWrapper:
@@ -92,7 +88,7 @@ class HyspexNavApplication:
         imageselection_window = controlfinder.find_child_window_by_title(
             window_title="Open")
         return imageselection_window
-        
+
     def _add_input_files(self, controlfinder: ControlFinder, input_folder: Path):
         """Add multiple input files via the Add Files window.
         Supported types: 
@@ -119,19 +115,16 @@ class HyspexNavApplication:
         open_btn = addfiles_cf.find_by_auto_id(control_type="Button",
                                                auto_id="1")
         open_btn.click()
-    
+
         # Wait for window to close
         addfiles_cf.window.wait_not('exists', timeout=DEFAULT_WAIT_TIME)
-
-
 
     def _clear_file_input(self, controlfinder: ControlFinder):
         """Clear the file input list in the main window."""
         btn = controlfinder.find_by_auto_id(
             control_type="Button",
-                auto_id="HySpexNav.splitter.layoutWidgetMain.btnInputFileClear")
+            auto_id="HySpexNav.splitter.layoutWidgetMain.btnInputFileClear")
         btn.invoke()
-
 
     def _set_output_folder(self, controlfinder: ControlFinder, output_folder: Path):
         """Set the output folder in the main window."""
@@ -149,16 +142,17 @@ class HyspexNavApplication:
     def _generate_output_files(self, controlfinder: ControlFinder):
         """Click the 'Generate Output' button to process the staged flight lines."""
         generate_btn = controlfinder.find_by_auto_id(
-            control_type="Button", auto_id="HySpexNav.splitter.layoutWidgetFlightLines.btnGenerateOutput")
+            control_type="Button",
+            auto_id="HySpexNav.splitter.layoutWidgetFlightLines.btnGenerateOutput")
         generate_btn.invoke()
 
     def _clear_flightlines(self, controlfinder: ControlFinder):
-        """Click the 'Clear Flight Lines' button to clear the staged flight lines for the next iteration."""
+        """Click the 'Clear Flight Lines' button to clear 
+        the staged flight lines for the next iteration."""
         clear_btn = controlfinder.find_by_auto_id(
-            control_type="Button", auto_id="HySpexNav.splitter.layoutWidgetFlightLines.btnFlightLineClear")
+            control_type="Button",
+            auto_id="HySpexNav.splitter.layoutWidgetFlightLines.btnFlightLineClear")
         clear_btn.invoke()
-    
-
 
     def run(self):
         """Run HySpexRad with the specified settings for all input/output folder pairs (= jobs."""
@@ -174,21 +168,23 @@ class HyspexNavApplication:
                     'enabled', timeout=DEFAULT_WAIT_TIME)
                 main_window_cf.window.set_focus()
                 # Reset Settings before next iteration
-                settings_path = get_base_path(__file__).parent /"configs" / "conf_hyspex_nav.ini" 
+                settings_path = get_base_path(
+                    __file__).parent / "configs" / "conf_hyspex_nav.ini"
                 self._load_settings(main_window_cf, settings_path)
-                #Next Iteration
-                self._set_output_folder(main_window_cf, output_folder)  
-                output_folder.mkdir(parents=True, exist_ok=True)  # ensure output folder exists
+                # Next Iteration
+                self._set_output_folder(main_window_cf, output_folder)
+                # ensure output folder exists
+                output_folder.mkdir(parents=True, exist_ok=True)
                 self._add_input_files(main_window_cf, input_folder)
                 self._analyze_input_files(main_window_cf)
-                time.sleep(1)  # Wait a bit to ensure analysis is done before generating output
+                # Wait a bit to ensure analysis is done before generating output
+                time.sleep(1)
                 self._generate_output_files(main_window_cf)
-                wait_for_file_count(folder=output_folder, expected_count=6, pattern="*.txt", timeout_s=10)  
-                #Clear for next iteration
-                self._clear_flightlines(main_window_cf)  
-                self._clear_file_input(main_window_cf)  
-                
-                
+                wait_for_file_count(
+                    folder=output_folder, expected_count=6, pattern="*.txt", timeout_s=10)
+                # Clear for next iteration
+                self._clear_flightlines(main_window_cf)
+                self._clear_file_input(main_window_cf)
 
 
 if __name__ == "__main__":
--- code/pipeline/runner.py
+++ code/pipeline/runner.py
@@ -9,7 +9,7 @@ Example:
 """
 
 from code.pipeline.steps import (
-    CopyJobFolders, BinaryToRadiance, MoveFiles, CopyFiles, 
+    CopyJobFolders, BinaryToRadiance, MoveFiles, CopyFiles,
     GeoreferenceSensors, ScrapeBaseStationData, FetchNavigationFiles,
     NavigationDiscretization, BuildDigitalSurfaceModel
 )
@@ -21,117 +21,160 @@ if __name__ == "__main__":
     print(f"Starting Pipeline for jobs: {selected_jobs}")
 
     # Define Steps
-    """Step 1: Fetch Raw Data
-        Some old data folders from 2025 contain already base station data ["rinex"] 
-        --> Exclude to make sure all base station data is freshly fetched by BasestationScraper and in the same format."""
+
+    # Step 1: Fetch Raw Data
+    #     Some old data folders from 2025 contain already base station data ["rinex"] 
+    #     --> Exclude to make sure all base station data is freshly fetched by 
+    #     BasestationScraper and in the same format.
     fetch_raw_data = CopyJobFolders(
         name="Fetch Raw Data",
-        input_folder=PipelineFolder(basefolder=Path("D:/data/mjolnir"), target= "01_raw_data"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "RAW"),
+        input_folder=PipelineFolder(
+            basefolder=Path("D:/data/mjolnir"),
+            target="01_raw_data"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="RAW"),
         jobs=selected_jobs,
-        exclude_within_output_folder=["rinex"] 
+        exclude_within_output_folder=["rinex"]
     )
-    """Step 2: Download Base Station Data from Swipos
-        Use the APX files in the raw data to get the flight time info for the scraper 
-        to know which base station data to download."""
+    # Step 2: Download Base Station Data from Swipos
+    #     Use the APX files in the raw data to get the flight time info for the scraper 
+    #     to know which base station data to download.
     download_base_station_data = ScrapeBaseStationData(
         name="Download Base Station Data",
         # Use data from APX folder to get flight time info for scraper
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "RAW/apx"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "RAW/rinex"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="RAW/apx"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="RAW/rinex"),
         jobs=selected_jobs
     )
-    """Step 3: Georeference Sensors"""
+    #Step 3: Georeference Sensors
     georeference_sensors = GeoreferenceSensors(
         name="Georeference Sensors (SWIR, VNIR, LiDAR)",
-        input_folder=PipelineFolder(basefolder= Path("E:/mjolnir_processing"), target="RAW"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="tmp"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="RAW"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp"),
         jobs=selected_jobs)
-    
-    """Step 4: Fetch Navigation Files"""
+
+    #Step 4: Fetch Navigation Files
     fetch_navigation_files = FetchNavigationFiles(
         name="Fetch Navigation Files",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target=""),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="tmp/input_hyspexnav"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target=""),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/input_hyspexnav"),
         jobs=selected_jobs)
-    
-    """Step 5: NavigationDiscretization"""
+
+    #Step 5: NavigationDiscretization
     navigation_discretization = NavigationDiscretization(
         name="Navigation Discretization",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="tmp/input_hyspexnav"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="tmp/swirvnir_data"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/input_hyspexnav"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/swirvnir_data"),
         jobs=selected_jobs
     )
 
-    """Step 6: Convert Binary to Radiance"""
+    #Step 6: Convert Binary to Radiance
     binary_to_radiance = BinaryToRadiance(
         name="Binary to Radiance",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "RAW"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "tmp/swirvnir_data"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="RAW"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/swirvnir_data"),
         jobs=selected_jobs
     )
-    """Step 7: Get VNIR Files"""
+    #Step 7: Get VNIR Files
     get_vnir_files = MoveFiles(
         name="Get VNIR Files",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "tmp/swirvnir_data"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target= "VNIR"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/swirvnir_data"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="VNIR"),
         jobs=selected_jobs,
-        regex_pattern=r"v1240" # v1240 for VNIR
-    ) 
-    
-    """Step 8: Get SWIR Files"""
+        regex_pattern=r"v1240"  # v1240 for VNIR
+    )
+
+    #Step 8: Get SWIR Files
     get_swir_files = MoveFiles(
         name="Get SWIR Files",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="tmp/swirvnir_data"),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="SWIR"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="tmp/swirvnir_data"),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="SWIR"),
         jobs=selected_jobs,
-        regex_pattern=r"s620"# s620 for VNIR
-    )  
-    """Step 9: Build Digital Surface Model (DSM) from LiDAR Point Cloud"""
+        regex_pattern=r"s620"  # s620 for VNIR
+    )
+    #Step 9: Build Digital Surface Model (DSM) from LiDAR Point Cloud
     build_digital_surface_model = BuildDigitalSurfaceModel(
         name="Build Digital Surface Model",
-        input_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target=""),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="DSM"),
+        input_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target=""),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="DSM"),
         jobs=selected_jobs
     )
-    """Step 10: Get SWIR Calibration Files
-         Note: Required as Parge Preparation"""
+    # Step 10: Get SWIR Calibration Files
+    #      Note: Required as Parge Preparation
     get_swir_calibration_files = CopyFiles(
         name="Get SWIR Calibration Files",
-        input_folder=PipelineFolder(basefolder=get_base_path(__file__).parent / "apps", target="calibration", static=True),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="SWIR"),
+        input_folder=PipelineFolder(
+            basefolder=get_base_path(__file__).parent / "apps",
+            target="calibration", static=True),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="SWIR"),
         jobs=selected_jobs,
         regex_pattern=r"boresight_swir|sensormodel")
-    
-    """Step 11: Get VNIR Calibration Files
-         Note: Required as Parge Preparation"""
+
+    # Step 11: Get VNIR Calibration Files
+    #      Note: Required as Parge Preparation
     get_vnir_calibration_files = CopyFiles(
         name="Get VNIR Calibration Files",
-        input_folder=PipelineFolder(basefolder=get_base_path(__file__).parent / "apps", target="calibration", static=True),
-        output_folder=PipelineFolder(basefolder=Path("E:/mjolnir_processing"), target="VNIR"),
+        input_folder=PipelineFolder(
+            basefolder=get_base_path(__file__).parent / "apps",
+            target="calibration",
+            static=True),
+        output_folder=PipelineFolder(
+            basefolder=Path("E:/mjolnir_processing"),
+            target="VNIR"),
         jobs=selected_jobs,
         regex_pattern=r"boresight_vnir|sensormodel"
     )
-    
-    
 
     # Run Steps
-    #fetch_raw_data.run()
-    #download_base_station_data.run()
-    #georeference_sensors.run()
+    # fetch_raw_data.run()
+    # download_base_station_data.run()
+    # georeference_sensors.run()
 
-    #fetch_navigation_files.run()
-    #navigation_discretization.run()
-    #binary_to_radiance.run()
-    #get_vnir_files.run()
-    #get_swir_files.run()
+    # fetch_navigation_files.run()
+    # navigation_discretization.run()
+    # binary_to_radiance.run()
+    # get_vnir_files.run()
+    # get_swir_files.run()
 
     build_digital_surface_model.run()
 
-    
-
     # Required for Parge (Georectification)
-    #get_swir_calibration_files.run()
-    #get_vnir_calibration_files.run()
+    # get_swir_calibration_files.run()
+    # get_vnir_calibration_files.run()
 
     print("All Pipeline steps completed.")
