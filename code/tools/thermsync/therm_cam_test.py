edeae9cb0cc0b4cb072c1cf03bc90e5dfea02d58
paACode <ackermann.pascal@gmail.com>
2026-05-06 16:36:32 +0200

    Remove script , approach with flirpy, not working

--- code/tools/thermsync/therm_cam_test.py
+++
@@ -1,9 +0,0 @@
-from flirpy.camera.tau import Tau
-
-try:
-    cam = Tau()
-    print(f"✅ Camera detected")
-    print(f"Available attributes: {[a for a in dir(cam) if not a.startswith('_')]}")
-    cam.close()
-except Exception as e:
-    print(f"❌ Error: {e}")
