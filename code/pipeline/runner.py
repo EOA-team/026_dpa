b600299bf37ddb86e1baf453620828a9ecf3fae3
paACode <ackermann.pascal@gmail.com>
2026-08-26 08:46:48 +0200

    chore: autopep runner.py

--- code/pipeline/runner.py
+++ code/pipeline/runner.py
@@ -22,15 +22,15 @@ from code.pipeline.steps import (
     GeoreferenceSensors,
     MoveFiles,
     NavigationDiscretization,
-    ScrapeBaseStationData,
     OrthomosaicToGeoTiff,
+    ScrapeBaseStationData,
 )
 
 NR_OF_FLIGHT_LINES = 5
 
 if __name__ == "__main__":
     selected_jobs = ["ZOFE_260522_80_5"]
-  
+
     print(f"Starting Pipeline for jobs: {selected_jobs}")
 
     # Define Steps
@@ -42,7 +42,8 @@ if __name__ == "__main__":
     fetch_raw_data = CopyJobFolders(
         name="Fetch Raw Data",
         input_folder=PipelineFolder(
-            basefolder=Path("Z:/drone/DERIS/data/RE/ZOFE/hypSpec"), # Important: use link to NAS at Reckenholz, not cached at Changins!
+            # Important: use link to NAS at Reckenholz, not cached at Changins!
+            basefolder=Path("Z:/drone/DERIS/data/RE/ZOFE/hypSpec"),
             target=""),
         output_folder=PipelineFolder(
             basefolder=Path("E:/mjolnir_processing"),
@@ -64,7 +65,7 @@ if __name__ == "__main__":
             target="RAW/rinex"),
         jobs=selected_jobs
     )
-    #Step 3: Georeference Sensors
+    # Step 3: Georeference Sensors
     georeference_sensors = GeoreferenceSensors(
         name="Georeference Sensors (SWIR, VNIR, LiDAR)",
         input_folder=PipelineFolder(
@@ -75,7 +76,7 @@ if __name__ == "__main__":
             target="tmp"),
         jobs=selected_jobs)
 
-    #Step 4: Fetch Navigation Files
+    # Step 4: Fetch Navigation Files
     fetch_navigation_files = FetchNavigationFiles(
         name="Fetch Navigation Files",
         input_folder=PipelineFolder(
@@ -86,7 +87,7 @@ if __name__ == "__main__":
             target="tmp/input_hyspexnav"),
         jobs=selected_jobs)
 
-    #Step 5: NavigationDiscretization
+    # Step 5: NavigationDiscretization
     navigation_discretization = NavigationDiscretization(
         name="Navigation Discretization",
         input_folder=PipelineFolder(
@@ -98,7 +99,7 @@ if __name__ == "__main__":
         jobs=selected_jobs
     )
 
-    #Step 6: Convert Binary to Radiance
+    # Step 6: Convert Binary to Radiance
     binary_to_radiance = BinaryToRadiance(
         name="Binary to Radiance",
         input_folder=PipelineFolder(
@@ -109,7 +110,7 @@ if __name__ == "__main__":
             target="tmp/swirvnir_data"),
         jobs=selected_jobs
     )
-    #Step 7: Get VNIR Files
+    # Step 7: Get VNIR Files
     get_vnir_files = MoveFiles(
         name="Get VNIR Files",
         input_folder=PipelineFolder(
@@ -122,7 +123,7 @@ if __name__ == "__main__":
         regex_pattern=r"v1240"  # v1240 for VNIR
     )
 
-    #Step 8: Get SWIR Files
+    # Step 8: Get SWIR Files
     get_swir_files = MoveFiles(
         name="Get SWIR Files",
         input_folder=PipelineFolder(
@@ -134,7 +135,7 @@ if __name__ == "__main__":
         jobs=selected_jobs,
         regex_pattern=r"s620"  # s620 for VNIR
     )
-    #Step 9: Build Digital Surface Model (DSM) from LiDAR Point Cloud
+    # Step 9: Build Digital Surface Model (DSM) from LiDAR Point Cloud
     build_digital_surface_model = BuildDigitalSurfaceModel(
         name="Build Digital Surface Model",
         input_folder=PipelineFolder(
@@ -173,7 +174,7 @@ if __name__ == "__main__":
         regex_pattern=r"boresight_vnir|sensormodel"
     )
     # Step 12: Geocoding
-    geocoding =  Geocoding(
+    geocoding = Geocoding(
         name="Geocoding",
         input_folder=PipelineFolder(
             basefolder=Path("E:/mjolnir_processing"),
@@ -182,7 +183,7 @@ if __name__ == "__main__":
             basefolder=Path("E:/mjolnir_processing"),
             target="output"),
         jobs=selected_jobs,
-        nr_of_flight_lines = NR_OF_FLIGHT_LINES
+        nr_of_flight_lines=NR_OF_FLIGHT_LINES
     )
     # Step 13: Create Radiance Orthomosaic
     create_radiance_orthomosaic = CreateRadianceOrthomosaic(
@@ -207,7 +208,7 @@ if __name__ == "__main__":
         jobs=selected_jobs
     )
 
-    # Step 15: Translate Orthomosaics to Geotiff 
+    # Step 15: Translate Orthomosaics to Geotiff
     orthomosaic_to_geotiff = OrthomosaicToGeoTiff(
         name="Translate Orthomosaics to Geotiff",
         input_folder=PipelineFolder(
@@ -218,7 +219,6 @@ if __name__ == "__main__":
             target="output/mosaics"),
         jobs=selected_jobs
     )
-
 
     # Run Steps
     fetch_raw_data.run()
@@ -238,9 +238,8 @@ if __name__ == "__main__":
     get_vnir_calibration_files.run()
 
     geocoding.run()
-    create_radiance_orthomosaic.run()   
+    create_radiance_orthomosaic.run()
     create_reflectance_orthomosaic.run()
     orthomosaic_to_geotiff.run()
-    
 
     print("All Pipeline steps completed.")
